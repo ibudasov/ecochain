649d9f7b4494dd9ceb613c2057b560efc24a27e1
Igor <igor.budasov@insided.com>
2017-04-18 17:48:17 +0200

    Docs

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -9,10 +9,24 @@ use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BlogController extends Controller
 {
     /**
+     * Returns the list of posts. In case of frontent it's known as an index page.
+     * Returns paginated data, feel free to specify pages.
+     *
+     * @ApiDoc(
+     *     resource=false,
+     *     resourceDescription="Operations on posts",
+     *     description="Retrieve list of posts.",
+     *     statusCodes={
+     *         200="Returned when successful"
+     *     }
+     * )
+     *
      * @Get("/blog.{_format}", name="front_blog", defaults={"_format" = "html"})
      * @Get("/post.{_format}", name="api_get_post", defaults={"_format" = "json"})
      * @View()
@@ -28,18 +42,47 @@ class BlogController extends Controller
     }
 
     /**
+     * Will return a post with specific id or throw an error
+     *
+     * @ApiDoc(
+     *     resource=false,
+     *     resourceDescription="Operations on posts",
+     *     description="Retrieve a particular post",
+     *     statusCodes={
+     *         200="Returned when successful",
+     *         404="Returned when not found"
+     *     }
+     * )
+     *
      * @Get("/read/{id}.{_format}", name="front_post", requirements={"id": "\d+"}, defaults={"_format" = "html"})
      * @Get("/post/{id}.{_format}", name="api_get_post_id", requirements={"id": "\d+"}, defaults={"_format" = "json"}))
      * @View()
      */
     public function viewAction(int $id): array
     {
+        $post = $this->getRepo()->find($id);
+
+        if(empty($post)) {
+            throw new NotFoundHttpException('Post not found');
+        }
+
         return [
-            'post' => $this->getRepo()->find($id)
+            'post' => $post
         ];
     }
 
     /**
+     * Returns list of posts, related to a query. Results will be paginated
+     *
+     * @ApiDoc(
+     *     resource=false,
+     *     resourceDescription="Operations on posts",
+     *     description="Returns list of posts, related to a query",
+     *     statusCodes={
+     *         200="Returned when successful"
+     *     }
+     * )
+     *
      * @QueryParam(name="query", requirements="\w+", description="Search query string")
      * @QueryParam(name="page", requirements="\d+", description="Page number", default="1")
      * @Get("/search.{_format}", name="front_search", defaults={"_format" = "html"})
@@ -62,6 +105,17 @@ class BlogController extends Controller
     }
 
     /**
+     * Returns list of posts, related to a query. Results will not be paginated, but quite limited
+     *
+     * @ApiDoc(
+     *     resource=false,
+     *     resourceDescription="Operations on posts",
+     *     description="Returns list of posts, related to a query",
+     *     statusCodes={
+     *         200="Returned when successful"
+     *     }
+     * )
+     *
      * @QueryParam(name="query", requirements="\w+", description="Search query string")
      * @Get("/post/liveSearch.{_format}", name="api_post_live_search", defaults={"_format" = "json"})
      * @View()
